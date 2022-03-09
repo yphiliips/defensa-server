33b5137103ea6cfeeee398686d44358bcd43123d
yphiliips <solisuriel@gmail.com>
2022-03-09 10:45:12 -0400

    BeUrielsp14

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,11 +19,11 @@ const firestore = getFirestore(firebase);
 const loginUser = async (req, res, next) => {
     try{
         const data = req.body;
-        res.send({
-            ok: true,
-            mensaje: 'Usuario Logueado.',
-            user: data
-        });
+        // res.send({
+        //     ok: true,
+        //     mensaje: 'Usuario Logueado.',
+        //     user: data
+        // });
         const userList = await getDocs(query(collection(firestore, 'users'), where('email', '==', data.email)));
         const usersArray = [];
         userList.forEach(doc => {
